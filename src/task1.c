6189285f89d4b4a2e570086e25d79e03afde84da
Aabriti Chapagain <aabritichapagain@gmail.com>
2026-07-11 09:02:02 +0000

    Add multithreading with pthreads

--- src/task1.c
+++ src/task1.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <pthread.h>
-
+#define NUM_THREADS 5
 void run_process_creation()
 {
     pid_t pid = fork();
@@ -25,11 +25,49 @@ void run_process_creation()
     }
 }
 
+void *task(void *arg)
+{
+    int id = *(int *)arg;
+
+    printf("Thread %d started (Thread ID: %lu)\n",
+           id,
+           (unsigned long)pthread_self());
+
+    usleep(100000 * id);   // Simulate work
+
+    printf("Thread %d finished\n", id);
+
+    return NULL;
+}
+void run_basic_threads()
+{
+    printf("\n========== Basic Multi-threading ==========\n");
+
+    pthread_t threads[NUM_THREADS];
+    int ids[NUM_THREADS] = {1, 2, 3, 4, 5};
+
+    // Create threads
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        pthread_create(&threads[i], NULL, task, &ids[i]);
+    }
+
+    // Wait for all threads
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("All threads have completed.\n");
+}
 int main()
 {
     printf("ST5004CEM Task 1\n");
 
     run_process_creation();
 
+    run_basic_threads();   
+
     return 0;
 }
+
